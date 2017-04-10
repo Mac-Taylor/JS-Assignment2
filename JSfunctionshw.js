7ea4f5d9a611db512ae6fa1499f98ecc7ff142a8
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-04-10 00:40:34 -0400

    putting file into repository

---
+++ JSfunctionshw.js
@@ -0,0 +1,71 @@
+
+// ** PROBLEM 1 | 'sum' function
+
+
+
+
+
+
+
+// ** PROBLEM 2 | 'avg' function
+
+
+
+
+
+
+
+// ** PROBLEM 3 | 'greaterThan' function
+
+
+
+
+
+
+
+// ** PROBLEM 4 | 'secondLargest' function
+
+
+
+
+
+
+
+// ** PROBLEM 5 | 'containsVowel' function
+
+
+
+
+
+
+
+// ** PROBLEM 6 | 'pigLatin' function 
+
+
+
+
+
+
+
+// ** PROBLEM 7 | 'longestWord' function
+
+let phrase = 'Rush is an amazing band';
+
+function longestWord(string) {
+
+let phraseSplit = string.split(' ');
+let longest = phraseSplit[0];
+
+for (let count = 0; count < phraseSplit.length; count++){
+    
+         if (phraseSplit[count].length > longest.length) {
+                longest = phraseSplit[count];
+         }    
+}
+
+return longest
+
+}
+
+let answer = longestWord(phrase);
+console.log (answer)
