eabd95aea8cbe647c1ca3d637dd71123ad0954bc
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-04-10 02:24:34 -0400

    avg function

--- JSfunctionshw.js
+++ JSfunctionshw.js
@@ -13,8 +13,8 @@ function sum(value1, value2) {
     return sum1;
 }
 
-let answer = sum(number1, number2);
-console.log(answer) 
+let answer1 = sum(number1, number2);
+console.log(answer1) 
 
 /* IN THE 'sum' FUNCTION ABOVE THE PARAMETERS ARE DEFINED BY CHANGING THE NUMERIC VALUE 
 IN THE DEFINITIONOF THE VARIABLE */
@@ -31,8 +31,8 @@ function sum(number1, number2) {
     return sum1;
 }
 
-let answer = sum(5, 6);
-console.log(answer) 
+let answer1 = sum(5, 6);
+console.log(answer1) 
 
 /* IN THIS VERSION OF THE 'sum' FUNCTION, THE PARAMERS ARE DEFINED 
 BY THE NUMERIC VALUES OF THE FUNCTION'S INPUTS. I WASN'T SURE WHICH 
@@ -42,10 +42,19 @@ WOULD BE MORE GENERAL OR USEFUL, SO PUT BOTH. */
 
 // ** PROBLEM 2 | 'avg' function
 
+let no1 = 0;
+let no2 = 0;
+let no3 = 0; 
+let average = 0;
 
+function avg(no1, no2, no3) {
+    average = (no1 + no2 + no3) / 3;
 
+    return average;
+}
 
-
+let answer2 = avg(45, 55, 65);
+console.log(answer2);
 
 // =================================================================================================
 
